a0f37b486a507502d86b3ac38685c44544d817c8
Кирилл Кувшинов <k.koovshinov@gmail.com>
2023-05-30 16:49:11 +0300

    Add some typing to them models under the ice_cream app.

--- anfisa_for_friends/ice_cream/models.py
+++ anfisa_for_friends/ice_cream/models.py
@@ -4,35 +4,35 @@ from core.models import PublishedModel
 
 
 class Category(PublishedModel):
-    title = models.CharField(max_length=256)
-    slug = models.SlugField(max_length=64, unique=True)
-    output_order = models.PositiveSmallIntegerField(default=100)
+    title: models.CharField = models.CharField(max_length=256)
+    slug: models.SlugField = models.SlugField(max_length=64, unique=True)
+    output_order: models.PositiveSmallIntegerField = models.PositiveSmallIntegerField(
+        default=100)
 
 
 class Topping(PublishedModel):
-    title = models.CharField(max_length=256)
-    slug = models.SlugField(max_length=64, unique=True)
+    title: models.CharField = models.CharField(max_length=256)
+    slug: models.SlugField = models.SlugField(max_length=64, unique=True)
 
 
 class Wrapper(PublishedModel):
-    title = models.CharField(max_length=256)
+    title: models.CharField = models.CharField(max_length=256)
 
 
 class IceCream(PublishedModel):
-    title = models.CharField(max_length=256)
-    description = models.TextField()
-    wrapper = models.OneToOneField(
+    title: models.CharField = models.CharField(max_length=256)
+    description: models.TextField = models.TextField()
+    wrapper: models.OneToOneField = models.OneToOneField(
         Wrapper,
         on_delete=models.SET_NULL,
         related_name='ice_cream',
         null=True,
         blank=True,
     )
-    category = models.ForeignKey(
+    category: models.ForeignKey = models.ForeignKey(
         Category,
         on_delete=models.CASCADE,
         related_name='ice_creams',
     )
-    toppings = models.ManyToManyField(Topping)
-    is_on_main = models.BooleanField(default=False)
-
+    toppings: models.ManyToManyField = models.ManyToManyField(Topping)
+    is_on_main: models.BooleanField = models.BooleanField(default=False)
